183e68cc28833dd4117137075d5fdf38213de3f0
silivalleydev <silivalleydev@gmail.com>
2022-06-07 20:28:47 +0900

    objec

--- 1-types/1-1-basic.ts
+++ 1-types/1-1-basic.ts
@@ -39,4 +39,39 @@
     // null: 비었다라고 결정된 의미 <-- 보통은 undefined를 사용
     let person:null;
     let person2: string | null;
+
+    // unknown <-- 가능하면 쓰지말것: 어떤타입일지 알 수 없다. <-- 자바스크립트 라이브러리를 쓸 때 어떤 것을 반환할지 모르는 경우 사용
+    let notSure: unknown = 0;
+    notSure = 'he';
+    notSure = true
+
+    // any <-- 가능하면 쓰지말것
+    let anything: any = 0;
+    anything = 'hello';
+
+    // void: 아무것도 리턴하지 않는 함수의 경우에 리턴 값으로 사용한다.
+    function print(): void {
+        console.log('hello');
+        return;
+    }
+
+    let unusable: void = undefined; //<-- 이렇게 사용안함
+
+    // never
+    function throwError(message: string): never {
+        // message -> server (log)
+        // 에러를 던지고 아무것도 리턴하지 않는경우
+        throw new Error(message);
+
+        // while문 무한정 도는경우
+        while (true) {}
+
+        // never 타입은 void도아닌 완전히 리턴되지 않는 경우
+    }
+
+    let obj: object; // 쓰지말것 <-- object도 명확하게 타입을 지정해줄것 왜냐하면 array가 올지 json이 올지 모르기 때문이다.
+
+    function acceptObject(obj: object) { console.log(obj) }
+    acceptObject({ name: 'hello' });
+    acceptObject([1, 2, 3, 4]);
 }
