d967fb359d6f0aba6cd2e30a3b90b37af88be369
silivalleydev <silivalleydev@gmail.com>
2022-06-19 08:35:25 +0900

    typescript

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -20,6 +20,24 @@
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
+  function printLoginState(loadState: ResourceLoadState) {
+    switch (loadState.state) {
+      case 'loading':
+        console.log('👀 loading...');
+        break;
+      case 'success':
+        // success일 땐 response의 body를 찍음
+        console.log(`😃 ${loadState.response.body}`);
+        break;
+      case 'fail':
+        // fail일 땐 reason을 찍음
+        console.log(`😱 ${loadState.reason}`); 
+        break;
+      default:
+        throw new Error(`unknown state: ${loadState}`);
+    }
+  }
+
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
