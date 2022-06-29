3c436cc1a5cfc76efd05cbddf9f820cd5dc50cf4
silivalleydev <silivalleydev@gmail.com>
2022-06-29 20:53:23 +0900

    coffee

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -7,7 +7,7 @@
     // 한샷에 7g의 커피빈이 필요하다고 가정
     const BEANS_GRAMM_PER_SHOT: number = 7;
     // 현재 보유한 커피 그램 수
-    let coffeeBeansGramm: number = 14;
+    let coffeeBeansGramm: number = 3 * BEANS_GRAMM_PER_SHOT;
     function makeCoffee(shots: number): CoffeeCup {
         // 커피빈이 충분하지 않은 경우 에러가 발생하도록 해준다.
         if (coffeeBeansGramm < shots * BEANS_GRAMM_PER_SHOT) {
