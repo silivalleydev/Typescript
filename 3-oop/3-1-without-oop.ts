9ae33e56788b0b9c6c038ba244de0066aee0ff85
silivalleydev <silivalleydev@gmail.com>
2022-06-29 20:50:10 +0900

    oop make coffee function

---
+++ 3-oop/3-1-without-oop.ts
@@ -0,0 +1,26 @@
+{
+    type CoffeeCup = {
+        shots: number; // 커피에 샷이 몇번 들어갔는지
+        hasMilk: boolean; // 커피에 우유가 들어갔는지
+    }
+
+    // 한샷에 7g의 커피빈이 필요하다고 가정
+    const BEANS_GRAMM_PER_SHOT: number = 7;
+    // 현재 보유한 커피 그램 수
+    let coffeeBeansGramm: number = 14;
+    function makeCoffee(shots: number): CoffeeCup {
+        // 커피빈이 충분하지 않은 경우 에러가 발생하도록 해준다.
+        if (coffeeBeansGramm < shots * BEANS_GRAMM_PER_SHOT) {
+            throw new Error('Not enough coffee beans!');
+        }
+        
+        coffeeBeansGramm -= shots * BEANS_GRAMM_PER_SHOT;
+        return {
+            shots,
+            hasMilk: false
+        }
+    }
+
+    const coffee = makeCoffee(2);
+    console.log(coffee);
+}
